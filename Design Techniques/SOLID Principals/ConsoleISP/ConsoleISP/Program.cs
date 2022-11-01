1fc7b091a079c7211be4f1c5f0ae7de01ceca1cd
Kavya Prasad <94643267+KavyaPrasad28@users.noreply.github.com>
2022-11-01 14:59:30 +0530

    Added ISP Project

---
+++ Design Techniques/SOLID Principals/ConsoleISP/ConsoleISP/Program.cs
@@ -0,0 +1,23 @@
+﻿using ISPLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleISP
+{
+    /// <summary>
+    /// The Interface Segregation Principle states "that clients should not be forced to implement interfaces they don't use. Instead of one fat interface, many small interfaces are preferred based on groups of methods, each one serving one submodule".
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IBorrowableDVD dvd = new DVD();
+            dvd.CheckOut("Lim");
+            Console.WriteLine(dvd.Borrower);
+            Console.WriteLine(dvd.BorrowDate);
+        }
+    }
+}
