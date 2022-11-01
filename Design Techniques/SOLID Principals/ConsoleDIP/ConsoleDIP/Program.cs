057887670a5ac2b983784fc97cf6b5fc925b9182
Kavya Prasad <94643267+KavyaPrasad28@users.noreply.github.com>
2022-11-01 15:01:09 +0530

    Added DIP Project

---
+++ Design Techniques/SOLID Principals/ConsoleDIP/ConsoleDIP/Program.cs
@@ -0,0 +1,34 @@
+﻿using DIPLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDIP
+{
+    /// <summary>
+    /// The Dependency Inversion Principle (DIP) states that high-level modules/classes should not depend on low-level modules/classes. Both should depend upon abstractions. Secondly, abstractions should not depend upon details. Details should depend upon abstractions.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPerson owner = Factory.CreatePerson();
+            owner.FirstName = "Kavya";
+            owner.LastName = "Prasad";
+            owner.EmailAddress = "kavya@gmail.com";
+            owner.PhoneNumber = "9442340920";
+
+            IChore chore = Factory.CreateChore();
+            chore.ChoreName = "Take out the trash";
+            chore.Owner = owner;
+
+
+
+            chore.PerformedWork(3);
+            chore.PerformedWork(1.5);
+            chore.CompleteChore();
+        }
+    }
+}
