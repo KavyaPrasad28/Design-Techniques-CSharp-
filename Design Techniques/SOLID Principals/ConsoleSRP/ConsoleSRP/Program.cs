cd98233ca16d4778614a24bd506cb94491124fa0
Kavya Prasad <94643267+KavyaPrasad28@users.noreply.github.com>
2022-11-01 14:52:13 +0530

    Added SRP Project

---
+++ Design Techniques/SOLID Principals/ConsoleSRP/ConsoleSRP/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSRP
+{
+    /// <summary>
+    /// SRP - Single Responsibility Pinciple
+    /// "A class should have only one job".
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StandardMessages.WelcomeMessage();
+
+            Person user = PersonDataCapture.Capture();
+
+            bool isUserValid = PersonValidator.Validate(user);
+
+            if(!isUserValid)
+            {
+                Console.Write("Press Enter to close...");
+                StandardMessages.EndApplication();
+                return;//here return closes the application.
+            }
+
+            AccountGenerator.CreateAccount(user);
+            Console.Write("Press Enter to close...");
+            StandardMessages.EndApplication();
+        }
+    }
+}
